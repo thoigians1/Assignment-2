642ec7a134fd29c89badacdaededf55e97de35d0
thoigians1 <trinhchu69@gmail.com>
2020-12-12 18:55:50 +0700

    Added AVL

---
+++ main.cpp
@@ -0,0 +1,234 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <class K, class V>
+class BKUTree {
+public:
+    class AVLTree;
+    class SplayTree;
+
+    class Entry {
+    public:
+        K key;
+        V value;
+
+        Entry(K key, V value) : key(key), value(value) {}
+    };
+
+private:
+    AVLTree* avl;
+    SplayTree* splay;
+    queue<K> keys;
+    int maxNumOfKeys;
+
+public:
+    BKUTree(int maxNumOfKeys = 5) {}
+    ~BKUTree() { this-> clear(); }
+
+    void add(K key, V value);
+    void remove(K key);
+    V search(K key, vector<K>& traversedList);
+
+    void traverseNLROnAVL(void (*func)(K key, V value));
+    void traverseNLROnSplay(void (*func)(K key, V value));
+
+    void clear();
+
+    class SplayTree {
+    public:
+        class Node {
+            Entry* entry;
+            Node* left;
+            Node* right;
+            typename AVLTree::Node* corr;
+
+            Node(Entry* entry = NULL, Node* left = NULL, Node* right = NULL) 
+            {
+                this->entry = entry;
+                this->left = left;
+                this->right = right;
+                this->corr = NULL;
+            }
+        };
+
+    public:
+        Node* root;
+
+        SplayTree(): root(NULL) {};
+        ~SplayTree() { this->clear(); };
+        
+        void add(K key, V value);
+        void add(Entry* entry);
+        void remove(K key);
+        V search(K key);
+
+        void traverseNLR(void (*func)(K key, V value));
+
+        void clear();
+    };
+
+    class AVLTree {
+    public:
+        class Node {
+            Entry* entry;
+            Node* left;
+            Node* right;
+            int balance;
+            int height;
+            typename SplayTree::Node* corr;
+
+            Node(Entry* entry = NULL, Node* left = NULL, Node* right = NULL) {
+                this->entry = entry;
+                this->left = left;
+                this->right = right;
+                this->balance = 0;
+                this->corr = NULL;
+            }
+        };
+
+    public:
+        Node* root;
+
+        AVLTree(): root(NULL) {};
+        ~AVLTree() { this->clear(); };
+        
+////////////////////////////////////////////////////////////////////////
+        
+        int height(Node *N)  
+		{  
+			if (N == NULL)  
+				return 0;  
+			return N->height;  
+		}  
+		
+		
+		// A utility function to get maximum 
+		// of two integers  
+		int max(int a, int b)  
+		{  
+			return (a > b)? a : b;  
+		}  
+        
+        
+        Node *rightRotate(Node *y)  
+		{  
+			Node *x = y->left;  
+			Node *T2 = x->right;  
+		  
+			// Perform rotation  
+			x->right = y;  
+			y->left = T2;  
+		  
+			// Update heights  
+			y->height = max(height(y->left), 
+							height(y->right)) + 1;  
+			x->height = max(height(x->left), 
+							height(x->right)) + 1;  
+		  
+			// Return new root  
+			return x;  
+		}  
+		
+		
+		Node *leftRotate(Node *x)  
+		{  
+			Node *y = x->right;  
+			Node *T2 = y->left;  
+		  
+			// Perform rotation  
+			y->left = x;  
+			x->right = T2;  
+		  
+			// Update heights  
+			x->height = max(height(x->left),     
+							height(x->right)) + 1;  
+			y->height = max(height(y->left),
+							height(y->right)) + 1;  
+		  
+			// Return new root  
+			return y;  
+		}  
+        
+        // Get Balance factor of node N  
+		int getBalance(Node *N)  
+		{  
+			if (N == NULL)  
+				return 0;  
+			return height(N->left) - height(N->right);  
+		}  
+        
+        // Recursive function to insert a key 
+		// in the subtree rooted with node and 
+		// returns the new root of the subtree.  
+		Node* insert(Node* node, int key)  
+		{  
+			/* 1. Perform the normal BST insertion */
+			if (node == NULL)  
+				return(newNode(key));  
+		  
+			if (key < node->key)  
+				node->left = insert(node->left, key);  
+			else if (key > node->key)  
+				node->right = insert(node->right, key);  
+			else // Equal keys are not allowed in BST  
+				return node;  
+		  
+			/* 2. Update height of this ancestor node */
+			node->height = 1 + max(height(node->left),  
+								height(node->right));  
+			
+			/* 3. Get the balance factor of this ancestor  
+				node to check whether this node became  
+				unbalanced */
+			int balance = getBalance(node);  
+		  
+			// If this node becomes unbalanced, then  
+			// there are 4 cases  
+		  
+			// Left Left Case  
+			if (balance > 1 && key < node->left->key)  
+				return rightRotate(node);  
+		  
+			// Right Right Case  
+			if (balance < -1 && key > node->right->key)  
+				return leftRotate(node);  
+		  
+			// Left Right Case  
+			if (balance > 1 && key > node->left->key)  
+			{  
+				node->left = leftRotate(node->left);  
+				return rightRotate(node);  
+			}  
+		  
+			// Right Left Case  
+			if (balance < -1 && key < node->right->key)  
+			{  
+				node->right = rightRotate(node->right);  
+				return leftRotate(node);  
+			}  
+		  
+			/* return the (unchanged) node pointer */
+			return node;  
+		}  
+        
+////////////////////////////////////////////////////////////////////////
+        
+        void add(K key, V value)
+        {
+			
+		};
+        void add(Entry* entry);
+        void remove(K key);
+        V search(K key);
+
+        void traverseNLR(void (*func)(K key, V value));
+
+        void clear();
+    };
+};
+
+int main() {
+	
+}
